4d93493ecd1acb6c2f7396a2ead8839bb5770a8b
Rahmonali Fatkhuddinov <rahmonali1995@gmail.com>
2023-04-11 14:51:34 +0500

    Initial Commit

---
+++ WebView/WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewApp.swift
+//  WebView
+//
+//  Created by Rahmonali on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
